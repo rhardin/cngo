df26791c4b40596642c0c9586de45fc7bf9c40dc
Hardin Rob <rob@hardin.nu>
2022-11-25 10:14:34 -0600

    monolith some more

--- cngo.go
+++ cngo.go
@@ -3,6 +3,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var store = make(map[string]string)
@@ -35,6 +40,73 @@ func Delete(key string) error {
 	return nil
 }
 
+// keyValuePutHandler exoects to be called from http PUT at
+// "/v1/key/{key}" resource.
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	val, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(val))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+// keyValueGetHandler expects to be called from http GET at
+// "/v1/key/{key}" resource.
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	val, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(val))
+}
+
+// keyValueDeleteHandler expects to be called from http DELETE at
+// "/v1/key/{key}" resource.
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	server()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
